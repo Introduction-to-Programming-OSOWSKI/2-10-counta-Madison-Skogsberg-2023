118d619432173230e6178c9d771542649e31ce14
Madison-Skogsberg-2023 <816418@mystma.org>
2021-10-06 09:13:20 -0500

    1

--- main.py
+++ main.py
@@ -1,1 +1,12 @@
 #WRITE YOUR CODE IN THIS FILE
+#define function
+#add parameters
+def countA(x):
+    x = x.lower()
+    z = 0
+    for i in range (0, len(x)):
+        if x[i] == 'a':
+            z = z + 1
+    return z
+#run function
+print(countA("apple"))
